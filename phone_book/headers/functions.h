144894aeed72074fc7c1b17673525aa7dace98b0
Makisto54 <ilja-martasov@mail.ru>
2021-08-26 20:29:04 +0700

    Update files

--- phone_book/headers/functions.h
+++ phone_book/headers/functions.h
@@ -6,6 +6,8 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 struct phone_book
 {
--- phone_book/src/functions.c
+++ phone_book/src/functions.c
@@ -1,4 +1,4 @@
-#include "functions.h"
+#include "../headers/functions.h"
 
 void add(struct phone_book *book)
 {
